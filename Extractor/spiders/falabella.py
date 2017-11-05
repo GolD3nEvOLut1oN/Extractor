7191a76c6b4a4f4b7f61382e92be8cc6c9a178b9
Rafael Rojas <rafaelrojas.cov@gmail.com>
2017-11-05 14:39:53 -0300

    commit inicial
    proyecto Scrapy
    Actualmente extrae datos de falabella y ripley
    
    Mejora continua

---
+++ Extractor/spiders/falabella.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+import json
+import scrapy
+import time
+from urllib.parse import urlencode, quote_plus
+import urllib
+from Extractor.items import Productos
+
+
+
+class TestingSpider(scrapy.Spider):
+	name = 'falabella'
+	api_url = 'http://www.falabella.com/rest/model/falabella/rest/browse/BrowseActor/get-product-record-list?'
+	allowed_domains = ['falabella.com']
+	start_urls = ['http://www.falabella.com']
+
+	def parse(self, response):
+		for menu_element in response.css('li.fb-masthead__grandchild-links__item'):
+			up_cat = str(menu_element.css('a::text').extract_first()).strip()
+			up_cat_url = str(menu_element.css('a::attr(href)').extract_first()).strip()
+			if up_cat and 'VER TODO' not in up_cat.upper():
+				up_cat_url = response.urljoin(up_cat_url)
+				#navegando por los links
+				request = scrapy.Request(url=up_cat_url, callback=self.getSubCategory)
+				request.meta['up_cat'] = up_cat
+				#request.meta['up_cat_url'] = up_cat_url
+				yield request
+
+
+	def getSubCategory(self,response):
+		up_cat = response.meta['up_cat']
+		up_cat_url = response.url #response.meta['up_cat_url']
+
+		if response.css('div.fb-hero-subnav--nav__block'):
+			for menu_element in response.css('div.fb-hero-subnav--nav__block'):
+				h3 = menu_element.css('h3::text').extract_first()
+				if h3 and 'VER TODO' not in str(h3).upper():
+					for sub_menu_element in menu_element.css('li'):
+						cat = str(sub_menu_element.css('a::text').extract_first()).strip()
+						cat_url = str(sub_menu_element.css('a::attr(href)').extract_first()).strip()
+
+						#Transformar el siguiente código a una funcion de tipo request
+						nextPage = "1"
+						curentUrl = cat_url
+						nextUrl = '{"currentPage":' + nextPage + ',"navState":"' + curentUrl +  '"}'
+						nextUrl = nextUrl.replace("http://www.falabella.com", "")
+						nextUrl = nextUrl.replace("/falabella-cl", "")
+						api_url = 'http://www.falabella.com/rest/model/falabella/rest/browse/BrowseActor/get-product-record-list?'
+						nextUrl = api_url + str(urllib.parse.quote(nextUrl, safe='~()*!.\''))
+						
+						head_req = {'Content-Type': 'application/json'}
+
+						request = scrapy.Request(url=nextUrl, callback=self.getProducts, method="GET", headers=head_req)
+
+						request.meta['up_cat'] = up_cat
+						request.meta['up_cat_url'] = up_cat_url
+						request.meta['cat'] = cat
+						request.meta['cat_url'] = response.urljoin(cat_url)
+
+						yield request
+
+				elif not h3:
+					cat = str(menu_element.css('a::text').extract_first()).strip()
+					cat_url = str(menu_element.css('a::attr(href)').extract_first()).strip()
+
+					nextPage = "1"
+					curentUrl = cat_url
+					nextUrl = '{"currentPage":' + nextPage + ',"navState":"' + curentUrl +  '"}'
+					nextUrl = nextUrl.replace("http://www.falabella.com", "")
+					nextUrl = nextUrl.replace("/falabella-cl", "")
+					api_url = 'http://www.falabella.com/rest/model/falabella/rest/browse/BrowseActor/get-product-record-list?'
+					nextUrl = api_url + str(urllib.parse.quote(nextUrl, safe='~()*!.\''))
+					
+					head_req = {'Content-Type': 'application/json'}
+
+					request = scrapy.Request(url=nextUrl, callback=self.getProducts, method="GET", headers=head_req)
+
+					request.meta['up_cat'] = up_cat
+					request.meta['up_cat_url'] = up_cat_url
+					request.meta['cat'] = cat
+					request.meta['cat_url'] = response.urljoin(cat_url)
+
+					yield request
+		else:
+			nextPage = "1"
+			curentUrl = response.url
+			nextUrl = '{"currentPage":' + nextPage + ',"navState":"' + curentUrl +  '"}'
+			nextUrl = nextUrl.replace("http://www.falabella.com", "")
+			nextUrl = nextUrl.replace("/falabella-cl", "")
+			api_url = 'http://www.falabella.com/rest/model/falabella/rest/browse/BrowseActor/get-product-record-list?'
+			nextUrl = api_url + str(urllib.parse.quote(nextUrl, safe='~()*!.\''))
+			
+			head_req = {'Content-Type': 'application/json'}
+
+			request = scrapy.Request(url=nextUrl, callback=self.getProducts, method="GET", headers=head_req)
+
+			request.meta['up_cat'] = up_cat
+			request.meta['up_cat_url'] = response.url
+			request.meta['cat'] = up_cat
+			request.meta['cat_url'] = response.url
+
+			yield request
+			
+	def getProducts(self,response):
+		
+		data = json.loads(response.text)
+
+		if data['success']:
+			for product in data['state']['resultList']:
+				item = Productos()
+				item['url'] = 'https://falabella.com' + product['url']
+				item['name'] = product['title']
+				for price in product['prices']:
+					if price['type'] and price['type'] == 3:
+						item['bprice'] = ''.join(x for x in price['originalPrice'] if x.isdigit())
+					elif price['type'] and price['type'] == 2:
+						item['price'] = ''.join(x for x in price['originalPrice'] if x.isdigit())
+					elif price['type'] and price['type'] == 1:
+						item['cprice'] = ''.join(x for x in price['originalPrice'] if x.isdigit())
+					else:
+						pass
+				item['date'] = time.strftime("%d/%m/%Y")
+				item['page'] = data['state']['curentPage']
+				item['cat_url'] = response.meta['cat_url']
+				item['up_category_url'] = response.meta['up_cat_url']
+				item['category'] = response.meta['cat']
+				item['up_category'] = response.meta['up_cat']
+
+				yield item
+
+			if int(data['state']['curentPage']) < int(data['state']['pagesTotal']):
+
+				nextPage = str(int(data['state']['curentPage']) + 1)
+				curentUrl = response.meta['cat_url']
+
+				nextUrl = '{"currentPage":' + nextPage + ',"navState":"' + curentUrl +  '"}'
+				nextUrl = nextUrl.replace("http://www.falabella.com", "")
+				nextUrl = nextUrl.replace("/falabella-cl", "")
+
+				api_url = 'http://www.falabella.com/rest/model/falabella/rest/browse/BrowseActor/get-product-record-list?'
+				nextUrl = api_url + str(urllib.parse.quote(nextUrl, safe='~()*!.\''))
+
+				head_req = {'Content-Type': 'application/json'}
+
+				if nextUrl is not None:
+					request = scrapy.Request(url=nextUrl, callback=self.getProducts, method="GET", headers=head_req)
+
+					request.meta['up_cat'] = response.meta['up_cat']
+					request.meta['up_cat_url'] = response.meta['up_cat_url']
+					request.meta['cat'] = response.meta['cat']
+					request.meta['cat_url'] = response.meta['cat_url']
+					yield request
+
+	#def putRequest(self,api_url,cat_url,up_cat_url,cat,up_cat,next_page)
---
+++ Extractor/spiders/ripley.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import time
+from Extractor.items import Productos
+
+
+class RipleySpider(scrapy.Spider):
+	name = 'ripley'
+	allowed_domains = ['simple.ripley.cl']
+	start_urls = ['http://simple.ripley.cl/']
+
+	def parse(self, response):
+		for menu_element in response.css('li.child-category'):
+			cat_name = str(menu_element.css('a::text').extract_first()).strip()
+			url = str(menu_element.css('a::attr(href)').extract_first()).strip()
+			if cat_name and cat_name.upper() != 'NONE' and 'VER TODO' not in cat_name.upper() and 'VER TODAS' not in cat_name.upper():
+				item = Productos()
+				item['category'] = cat_name
+				item['cat_url'] = response.urljoin(url)
+				#navegando por los links
+				url = response.urljoin(url)
+				request = scrapy.Request(url=url, callback=self.getProducts)
+				request.meta['item'] = item
+				yield request
+
+
+	def getProducts(self,response):
+		item = response.meta['item']
+
+		for product in response.css('.catalog-product'):
+			url_product = str(product.css('.catalog-product::attr(href)').extract_first()).strip()
+			img_product = str(product.css('img::attr(data-src)').extract_first()).strip()
+			name_product = str(product.css('.catalog-product-name::text').extract_first()).strip()
+			normal_price = str(product.css('span.catalog-product-list-price::text').extract_first()).strip()
+			best_price = str(product.css('span.catalog-product-offer-price::text').extract_first()).strip()
+			card_price = str(product.css('span.catalog-product-card-price::text').extract_first()).strip()
+
+			normal_price = ''.join(x for x in normal_price if x.isdigit())
+			best_price = ''.join(x for x in best_price if x.isdigit())
+			card_price = ''.join(x for x in card_price if x.isdigit())
+
+			item['url'] = 'https://simple.ripley.cl' + url_product
+			item['img'] = 'https:' + img_product
+			item['name'] = name_product
+			item['price'] = normal_price
+			item['bprice'] = best_price
+			item['cprice'] = card_price
+			item['date'] = time.strftime("%d/%m/%Y")
+
+			yield item
+
+		next_page_url = response.xpath('//ul[@class="pagination"]//li[position() = (last())]/a/@href').extract_first()
+		if next_page_url is not None:
+			next_page_url = response.urljoin(next_page_url)
+			request = scrapy.Request(url=next_page_url, callback=self.getProducts)
+			request.meta['item'] = item
+			yield request
+
